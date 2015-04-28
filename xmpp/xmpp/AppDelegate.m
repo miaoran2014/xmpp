8f6dc97aa06f4ccc2272e8a788514c8c8a70554a
miaoran <1822773181@qq.com>
2015-04-28 17:21:34 +0800

    1:项目搭建

---
+++ xmpp/xmpp/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  xmpp
+//
+//  Created by admin on 15/4/28.
+//  Copyright (c) 2015年 admin. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "XMPPFramework.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+@end
---
+++ xmpp/xmpp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xmpp
+//
+//  Created by admin on 15/4/28.
+//  Copyright (c) 2015年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
